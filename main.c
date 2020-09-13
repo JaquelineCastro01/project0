51c729dc7a49d828fcc0a0800053cce2fb7a3c3a
JaquelineCastro01 <49821292+JaquelineCastro01@users.noreply.github.com>
2020-09-13 15:15:29 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,121 +1,107 @@
-/*
-About this program:
-- This program counts words.
-- The specific words that will be counted are passed in as command-line
-  arguments.
-- The program reads words (one word per line) from standard input until EOF or
-  an input line starting with a dot '.'
-- The program prints out a summary of the number of times each word has
-  appeared.
-- Various command-line options alter the behavior of the program.
-
-E.g., count the number of times 'cat', 'nap' or 'dog' appears.
-> ./main cat nap dog
-Given input:
- cat
- .
-Expected output:
- Looking for 3 words
- Result:
- cat:1
- nap:0
- dog:0
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "smp0_tests.h"
 
-#define LENGTH(s) (sizeof(s) / sizeof(*s))
-
-/* Structures */
 typedef struct {
-  char *word;
-  int counter;
+char *word;
+int counter;
 } WordCountEntry;
 
-
-int process_stream(WordCountEntry entries[], int entry_count)
+int process_stream (WordCountEntry entries[], int entry_count)
 {
-  short line_count = 0;
-  char buffer[30];
+short line_count = 0;
+char buffer[30];
+while (fgets (buffer, sizeof (buffer), stdin)) {
+if (*buffer == '.')
+break;
 
-  while (gets(buffer)) {
-    if (*buffer == '.')
-      break;
-    /* Compare against each entry */
-    int i = 0;
-    while (i < entry_count) {
-      if (!strcmp(entries[i].word, buffer))
-        entries[i].counter++;
-      i++;
-    }
-    line_count++;
-  }
-  return line_count;
+size_t len = strlen (buffer);
+if (buffer[len - 1] == '\n')
+buffer[--len] = '\0'; //null character at the end
+entries[line_count].word[strlen(entries[line_count].word)]='\0';
+
+if (strcmp (entries[line_count].word, buffer)==0)
+entries[line_count].counter++;
+++line_count;
+if (line_count == entry_count)
+break;
+}
+return line_count;
 }
 
-
-void print_result(WordCountEntry entries[], int entry_count)
+void print_result (WordCountEntry entries[], int entry_count)
 {
-  printf("Result:\n");
-  while (entry_count-- > 0) {
-    printf("%s:%d\n", entries->word, entries->counter);
-  }
+fprintf (stdout, "\nResult:\n");
+int k;
+for(k = 0; k < entry_count; k++) {
+printf ("%s:%d\n", entries[k].word, entries[k].counter);
+}
 }
 
-
-void printHelp(const char *name)
+void printHelp (const char *name)
 {
-  printf("usage: %s [-h] <word1> ... <wordN>\n", name);
+fprintf (stderr, "usage: %s [-h] [-f FILENAME] <word1> ... <wordN>\n", name);
 }
 
-
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-  const char *prog_name = *argv;
-
-  WordCountEntry entries[5];
-  int entryCount = 0;
-
-  /* Entry point for the testrunner program */
-  if (argc > 1 && !strcmp(argv[1], "-test")) {
-    run_smp0_tests(argc - 1, argv + 1);
-    return EXIT_SUCCESS;
-  }
-
-  while (*argv != NULL) {
-    if (**argv == '-') {
-
-      switch ((*argv)[1]) {
-        case 'h':
-          printHelp(prog_name);
-        default:
-          printf("%s: Invalid option %s. Use -h for help.\n",
-                 prog_name, *argv);
-      }
-    } else {
-      if (entryCount < LENGTH(entries)) {
-        entries[entryCount].word = *argv;
-        entries[entryCount++].counter = 0;
-      }
-    }
-    argv++;
-  }
-  if (entryCount == 0) {
-    printf("%s: Please supply at least one word. Use -h for help.\n",
-           prog_name);
-    return EXIT_FAILURE;
-  }
-  if (entryCount == 1) {
-    printf("Looking for a single word\n");
-  } else {
-    printf("Looking for %d words\n", entryCount);
-  }
-
-  process_stream(entries, entryCount);
-  print_result(entries, entryCount);
-
-  return EXIT_SUCCESS;
+const char *prog_name = *argv;
+WordCountEntry entries[argc];
+//intialize all the entris
+for(int i=1;i<argc;++i)
+{
+   entries[i].word=NULL;
+   entries[i].counter=0;
 }
+int entryCount = 0;
+
+for(int i = 1; i < argc; i++) {
+if (*argv[i] == '-') {
+  
+switch (argv[i][1]) {
+case 'h':
+printHelp (prog_name);
+break;
+case 'f':
+freopen((argv)[2],"w",stdout);
+break;
+default:
+fprintf (stderr, "%s: Invalid option %s. Use -h for help.\n",prog_name, *argv);
+}
+}
+else {
+bool flag=false;
+//check if word already exists in the word count entry
+for(int k=0;k<entryCount;++k)
+{
+   if(strcmp(entries[k].word,argv[i])==0)
+       flag=true;
+}
+//only add if doesnt exists alredy
+if(flag==false)
+{
+
+entries[entryCount].word = argv[i];
+entries[entryCount++].counter = 0;
+}
+}
+}
+
+if (!entryCount) {
+fprintf (stderr,"%s: Please supply at least one word. Use -h for help.\n",prog_name);
+return EXIT_FAILURE;
+}
+
+if (entryCount == 1) {
+fprintf (stdout, "Looking for a single word\n");
+}
+else {
+fprintf (stdout, "Looking for %d words\n", entryCount);
+}
+process_stream (entries, entryCount);
+print_result (entries, entryCount);
+
+return EXIT_SUCCESS;
+fflush(stdout);
+}
+
